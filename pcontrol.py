762eb3c530f64ef3a1569123afab1228d4310d5e
Christian Ricci <riccic@amazon.com>
2020-03-18 23:38:14 +0000

    feature: separated dns entries from logs

--- pcontrol.py
+++ pcontrol.py
@@ -4,10 +4,15 @@ import sqlite3
 import re
 
 def print_and_accept(pkt):
-    scapy_pkt = IP(pkt.get_payload())
-    src = scapy_pkt.src  # source IP address
-    dst = scapy_pkt.dst
-    dns_answer = scapy_pkt
+    packet = IP(pkt.get_payload())
+    src = packet.src
+    dstport = packet[UDP].dport
+    hw = pkt.get_hw()
+    haddr = ''
+    if hw and type(hw[0]) != int:
+        haddr = ":".join("{:02x}".format(ord(c)) for c in hw[0:6])
+
+    dns_answer = packet
     full_dns_list = ''
     if dns_answer and DNS in dns_answer:
         for x in range(dns_answer[DNS].ancount):
@@ -17,50 +22,55 @@ def print_and_accept(pkt):
                 (rdata.decode('utf8') if type(rdata) == bytes else rdata)
             if (x + 1) < dns_answer[DNS].ancount:
                 full_dns_list += '|'
-        hw = pkt.get_hw()
-        haddr = ''
-        if hw and type(hw[0]) != int:
-            haddr = ":".join("{:02x}".format(ord(c)) for c in hw[0:6])
-
-        pkt.accept()
-
         re_c = re.compile(r'[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*')
         for ip in list(filter(re_c.search, [alias.split(':')[-1] for alias in full_dns_list.split('|')])):
-            values = "'{src}','{dst}','{haddr}','{dns_query_name}','{dns_query_ip}','{full_dns_alias_tree}'"\
-                .format(src=src,
-                        dst=dst,
-                        haddr=haddr,
-                        dns_query_name=full_dns_list.split(':')[0],
+            values = "'{dns_query_name}','{dns_query_ip}','{full_dns_alias_tree}'"\
+                .format(dns_query_name=full_dns_list.split(':')[0],
                         dns_query_ip=ip,
                         full_dns_alias_tree=full_dns_list)
-            add_row(cursor, values)
-            print(values)
+            add_row(cursor, values, dns_table_name)
+            print('# DNS: ', values)
+            values = "'{src}','{haddr}'"\
+                .format(src=src,
+                        haddr=haddr)
+            add_row(cursor, values, log_table_name)
+            print('# LOG: ', values)
+    pkt.accept()
 
-def connect(sqlite_file, table_name):
-    """ Make connection to an SQLite database file """
+def connect(sqlite_file):
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
-    c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=table_name))
-    c.execute('CREATE TABLE IF NOT EXISTS {tn} (src text, dst text, haddr text, dns_query_name text, \
-               dns_query_ip text, full_dns_alias_tree text, PRIMARY KEY(src, dst, haddr, dns_query_name, dns_query_ip))' \
-              .format(tn=table_name))
+    c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=dns_table_name))
+    c.execute('CREATE TABLE IF NOT EXISTS {tn} ( \
+                dns_query_name text, \
+                dns_query_ip text, \
+                full_dns_alias_tree text, \
+                PRIMARY KEY(dns_query_name, dns_query_ip))'\
+              .format(tn=dns_table_name))
+    c.execute('DROP TABLE IF EXISTS {tn}'.format(tn=log_table_name))
+    c.execute('CREATE TABLE IF NOT EXISTS {tn} ( \
+                src text, \
+                dst text, \
+                dstport text, \
+                log_text text)'\
+              .format(tn=log_table_name))
     return conn, c
 
 def close(conn):
-    """ Commit changes and close connection to the database """
-    # conn.commit()
     conn.close()
 
-def add_row(cursor, values):
+def add_row(cursor, values, table_name):
     try:
-        cursor.execute('INSERT INTO {tn} values ({values})'.format(tn=table_name, values=values))
+        cursor.execute('INSERT INTO {tn} values ({values})'.format(
+            tn=table_name, values=values))
     except sqlite3.IntegrityError:
         pass
     conn.commit()
 
 sqlite_file = '/tmp/pcontrol.sqlite'
-table_name = 'dns_query_tab'
-conn, cursor = connect(sqlite_file, table_name)
+dns_table_name = 'dns_query_tab'
+log_table_name = 'log_tab'
+conn, cursor = connect(sqlite_file)
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(0, print_and_accept)
